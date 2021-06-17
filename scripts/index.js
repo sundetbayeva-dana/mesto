26d1dc5c770ff825e9bbdd9ff0f470d17fdaab95
Dana <sundetbayeva.dana@yandex.ru>
2021-06-17 17:57:44 +0500

    developing validation 1

--- scripts/index.js
+++ scripts/index.js
@@ -93,47 +93,71 @@ function handlerFormProfileSubmit(event) {
 
 
 ////////////////////////////
-const formPlace = document.querySelector('.popup__form-place');
+/*const formPlace = document.querySelector('.popup__form-place');
 const formInputPlace = formPlace.querySelector('.popup__form-text');
-const formError = formPlace.querySelector(`.${formInputPlace.id}-error`);
+const formError = formPlace.querySelector(`.${formInputPlace.id}-error`);*/
 
 
 /*formInputPlace.addEventListener('input', function(event) {
   console.log(event.target.validity);
 })*/
 
-const showError = (input, errorMessage) => {
-  input.classList.add('popup__form-text-error');
-  formError.textContent = errorMessage;
-  formError.classList.add('popup__form-text-error_active')
+//const showError = (input, errorMessage) => {
+const showError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__form-text-error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__form-text-error_active');
+  
 }
 
-const hideError = (input) => {
-  input.classList.remove('popup__form-text-error');
+const hideError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__form-text-error');
+  errorElement.classList.remove('popup__form-text-error_active');
+  errorElement.textContent = ''
 };
 
-const checkInputValidity = () => {
+const checkInputValidity = (formElement, inputElement) => {
 
-  if (!formInputPlace.validity.valid) {
-    showError(formInputPlace, formInputPlace.validationMessage);
+  if (!inputElement.validity.valid) {
+    showError(formElement, inputElement, inputElement.validationMessage);
    } else {    
-    hideError(formInputPlace) 
+    hideError(formElement, inputElement) 
   };
-  const currentLength = formInputPlace.value.length;
-  console.log(currentLength)
   
 };
 
 
-
-
-formPlace.addEventListener('submit', function (event) {
+/*formPlace.addEventListener('submit', function (event) {
   event.preventDefault();
 })
 
 formInputPlace.addEventListener('input', function () {
-  checkInputValidity();
-});
+  checkInputValidity(formPlace, formInputPlace);
+});*/
+
+const setEventListeners = (formElement) => {
+  const inputList =  Array.from(formElement.querySelectorAll('.popup__form-text'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function() {
+      checkInputValidity(formElement, inputElement);
+    })
+  })
+}
+
+
+
+function enableValidation() {
+  let formList = Array.from(document.querySelectorAll('.popup__form-place'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+    });
+    setEventListeners(formElement);
+  })
+ }
+ enableValidation();
 
 ////////////////////////////
 
